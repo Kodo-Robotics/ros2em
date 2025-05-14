8652a1589bfcbc8efbdc821db2d8837bcd786779
Sakshay Mahna <sakshum19@gmail.com>
2025-05-14 13:03:39 +0530

    Fix bugs to Vagrantfile

--- ros2em/core/vagrantfile_gen.py
+++ ros2em/core/vagrantfile_gen.py
@@ -2,9 +2,10 @@ def generate_vagrantfile(env_path, name, host_ports, cpu, ram):
     content = f"""
 Vagrant.configure("2") do |config|
     config.vm.define "{name}" do |vm|
-        vm.vm.box = ros2em/ros2-humble"
+        vm.vm.box = "ros2em/ros2-humble"
         vm.vm.hostname = "{name}"
 
+        vm.vm.network "public_network"
         vm.vm.network "forwarded_port", guest: 6080, host: {host_ports['vnc']}, host_ip: "127.0.0.1"
         vm.vm.boot_timeout = 600
         
